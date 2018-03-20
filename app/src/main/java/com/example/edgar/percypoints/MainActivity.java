df29d367f1c6aae3591991db9d730208cd1d9a10
Michelle Loo <michelle.loo@cibc.live>
2018-03-20 14:38:13 -0400

    Rough Quiz

--- app/src/main/java/com/example/edgar/percypoints/MainActivity.java
+++ app/src/main/java/com/example/edgar/percypoints/MainActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    static final int QUIZ_TIME = 1;  // The request code
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
     /** Called when the user taps the Send button */
     public void startQuiz(View view) {
         Intent intent = new Intent(this, Quiz.class);
-        startActivity(intent);
+
+        startActivityForResult(intent, QUIZ_TIME);
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == QUIZ_TIME) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                TextView numPoints = (TextView) findViewById(R.id.numPoints);
+                int number = Integer.parseInt(numPoints.getText().toString());
+                number += 4;
+                numPoints.setText(Integer.toString(number));
+            }
+        }
     }
 }
