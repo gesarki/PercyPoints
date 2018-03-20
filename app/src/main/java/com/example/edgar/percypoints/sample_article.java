25fa477742218b5a2dfec5e1ed8aa6f518f33e7b
Jared <jparcill@edu.uwaterloo.ca>
2018-03-20 15:05:17 -0400

    just cause

--- app/src/main/java/com/example/edgar/percypoints/sample_article.java
+++ app/src/main/java/com/example/edgar/percypoints/sample_article.java
@@ -1,11 +1,8 @@
 package com.example.edgar.percypoints;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 public class sample_article extends AppCompatActivity {
 
@@ -17,16 +14,5 @@ public class sample_article extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
-
-
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 }
