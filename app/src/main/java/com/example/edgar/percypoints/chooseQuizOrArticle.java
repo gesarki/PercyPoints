443d73cafdcfc767eb087ddd7026e71ff612a697
edgar <gesarki@gmail.com>
2018-03-20 14:31:55 -0400

    added activity chooser screen

--- app/src/main/java/com/example/edgar/percypoints/chooseQuizOrArticle.java
+++ app/src/main/java/com/example/edgar/percypoints/chooseQuizOrArticle.java
@@ -4,23 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class chooseQuizOrArticle extends AppCompatActivity {
+
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_choose_quiz_or_article);
+
+        //imageView = (ImageView) findViewById(R.id.first_card_image);
+        //imageView.setBackgroundColor(000000);
+
+
     }
-    /** Called when the user taps the Send button */
+
     public void startQuiz(View view) {
         Intent intent = new Intent(this, Quiz.class);
         startActivity(intent);
     }
-    public void startExtraPts(View view) {
-        Intent intent = new Intent(this, chooseQuizOrArticle.class);
-        startActivity(intent);
-    }
 
 
-    }
+}
