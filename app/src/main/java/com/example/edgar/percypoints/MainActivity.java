e358b0d6e8094fa8e9fcd47f22d7e61ccb8b9cf5
Jared <jparcill@edu.uwaterloo.ca>
2018-03-21 11:27:44 -0400

    more minor changes to be inline with master branch

--- app/src/main/java/com/example/edgar/percypoints/MainActivity.java
+++ app/src/main/java/com/example/edgar/percypoints/MainActivity.java
@@ -1,17 +1,10 @@
 package com.example.edgar.percypoints;
 
 import android.content.Intent;
-<<<<<<< HEAD
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-=======
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
->>>>>>> master
 
 public class MainActivity extends AppCompatActivity {
     static final int QUIZ_TIME = 1;  // The request code
@@ -21,14 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-<<<<<<< HEAD
 
-    public void open_sample_article(View view) {
-        Intent intent = new Intent(this, sample_article.class);
-        startActivity(intent);
-    }
-}
-=======
+    // for future implimentation of article card
+    //public void openSampleArticle(View view) {
+    //    Intent intent = new Intent(this, sample_article.class);
+    //    startActivity(intent);
+    //}
+
     /** Called when the user taps the Send button */
     public void startQuiz(View view) {
         Intent intent = new Intent(this, Quiz.class);
@@ -54,5 +46,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    }
->>>>>>> master
+}
